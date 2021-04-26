03f21278b8a1aadc0f6c639e8ed605e0b818b926
jcameron10 <jovonnec@gmail.com>
2021-04-26 19:02:38 -0400

    migrated tables

--- db/migrate/20210426210923_create_homes.rb
+++ db/migrate/20210426210923_create_homes.rb
@@ -1,13 +1,13 @@
 class CreateHomes < ActiveRecord::Migration[6.1]
   def change
     create_table :homes do |t|
-      t.references :user, null: true, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :img_url
       t.string :description
-      t.int :rate
-      t.int :num_beds
-      t.int :num_baths
-      t.int :avg_rating
+      t.integer :rate
+      t.integer :num_beds
+      t.integer :num_baths
+      t.integer :avg_rating
 
       t.timestamps
     end
--- db/migrate/20210426215940_create_bookings.rb
+++ db/migrate/20210426215940_create_bookings.rb
@@ -1,11 +1,11 @@
 class CreateBookings < ActiveRecord::Migration[6.1]
   def change
     create_table :bookings do |t|
-      t.references :user, null: true, foreign_key: true
-      t.references :home, null: true, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :home, null: false, foreign_key: true
       t.date :start_date
       t.date :end_date
-      t.int :price
+      t.integer :price
 
       t.timestamps
     end
