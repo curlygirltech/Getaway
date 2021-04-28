bb519839d2f5e11eb380dd9ed6232f8fffaf7193
jcameron10 <jovonnec@gmail.com>
2021-04-28 12:59:44 -0400

    material Ui depenceies and folder creation

---
+++ client/containers/MainContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function MainContainer() {
+  return (
+    <div>
+      <Switch>
+        <Route path= '/bookings'>
+        </Route>
+        <Route path= '/homes'>
+        </Route>
+        <Route path='/users'>
+        </Route>
+      </Switch>
+    </div>
+  )
+}
---
+++ client/services/bookings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function bookings() {
+
+
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
