93dad606ddf88bc2fadd0630d2bdef0adcf006af
jcameron10 <jovonnec@gmail.com>
2021-04-29 12:09:20 -0400

    register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,42 @@
-import './App.css';
-import { Route, Switch } from 'react-router-dom'
-import {useState} from 'react'
+import "./App.css";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { useState } from "react";
 // import Nav from './Layout/Nav'
-import Layout from './Layout/Layout'
-import Login from './screens/Login'
-import { loginUser } from './services/auth';
+import Layout from "./Layout/Layout";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import { loginUser, registerUser } from "./services/auth";
 
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null)
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const history = useHistory();
 
   const handleLogin = async (formData) => {
-    const userData = await loginUser(formData)
-    setCurrentUser(userData)
-  }
+    const userData = await loginUser(formData);
+    setCurrentUser(userData);
+    history.push("/");
+  };
+
+  const handleRegister = async (formData) => {
+    const userData = await registerUser(formData);
+    setCurrentUser(userData);
+    history.push("/");
+  };
+
   return (
     <div className="App">
-    <Layout>
-      <Switch>
-          <Route path='/login'>
+      <Layout>
+        <Switch>
+          <Route path="/login">
             <Login handleLogin={handleLogin} />
           </Route>
-      </Switch>
-      
-    </Layout>
-
+          <Route path="/register">
+            <Register handleRegister={ handleRegister } />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
